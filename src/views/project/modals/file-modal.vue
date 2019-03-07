743240d69a62a5c783b6d6b2a91155e4c3c698e8
SmaVivian <vivi163abc@163.com>
2019-03-07 10:59:22 +0800

    模拟数据

--- src/views/project/modals/file-modal.vue
+++ src/views/project/modals/file-modal.vue
@@ -7,6 +7,7 @@
     v-model="visible"
     @ok="handleOk">
     <div>
+      <div></div>
       <p>名称1</p>
       <p>名称1</p>
       <p>名称1</p>
@@ -20,7 +21,47 @@
 export default {
   data() {
     return {
-      visible: false
+      visible: false,
+      dataList: [
+        {
+          name: '烈火展项目',
+          children: [
+            {
+              name: '招标文件夹',
+              children: [
+                {
+                  name: '招标文件夹1',
+                },
+                {
+                  name: '招标文件夹2',
+                },
+              ]
+            },
+            {
+              name: '设计文件夹'
+            },
+          ]
+        },
+        {
+          name: '东北抗联展项目',
+          children: [
+            {
+              name: '东北招标文件夹',
+            },
+            {
+              name: '东北设计文件夹',
+              children: [
+                {
+                  name: '招标文件夹1',
+                },
+                {
+                  name: '招标文件夹2',
+                },
+              ]
+            },
+          ]
+        }
+      ]
     }
   },
   methods: {
